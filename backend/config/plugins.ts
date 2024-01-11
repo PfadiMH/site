a9a337c5933af49769139793a6ae1a3d1a655548
Silvio Brändle <silvio@silviobraendle.ch>
2024-01-11 15:30:13 +0000

    Add GraphQL plugin to Strapi

---
+++ backend/config/plugins.ts
@@ -0,0 +1,14 @@
+export default {
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
+    },
+  },
+};
