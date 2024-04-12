626ce1ef9d83b197eac44a8d7129274cdc9d4830
Battino <battino46@gmail.com>
2024-04-12 12:33:02 +0000

    add logo to Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { FooterColumnsBuilder } from "./FooterColumn";
 import prisma, { Prisma } from "@/lib/prisma";
+import Image from "next/image";
+import { ImageComponent } from "../Shared/ImageComponent";
+import { getAssetPath } from "@/lib/getAssetInfo";
 
 export type FooterProps = Prisma.FooterGetPayload<{}> & {
   footerColumnsSlot: React.ReactNode;
@@ -14,6 +17,7 @@ export async function Footer({
   return (
     <footer>
       <div className="grid grid-cols-2 gap-4">{footerColumnsSlot}</div>
+      {logo && <ImageComponent path={await getAssetPath(logo)} title="Logo" />}
       {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
     </footer>
   );
