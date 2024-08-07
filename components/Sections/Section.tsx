4ba9da5a7f3858f485724d4cf6df1de239865b7c
Silvio Braendle <silvio@silviobraendle.ch>
2024-08-07 14:07:00 +0000

    Add section breaks

--- components/Sections/Section.tsx
+++ components/Sections/Section.tsx
@@ -8,12 +8,27 @@ interface SectionProps {
 
 function Section({ contentSlot, index }: SectionProps) {
   const isMud = index % 2 === 0;
+  const isSun = index % 2 === 1;
 
   const theme = isMud ? "mud" : "sun";
 
   return (
     <div className={`${theme}-theme`}>
+      {isSun && (
+        <object
+          className="w-full"
+          type="image/svg+xml"
+          data="/section-break-presun.svg"
+        />
+      )}
       <ThemeProvider theme={theme}>{contentSlot}</ThemeProvider>
+      {isSun && (
+        <object
+          className="w-full"
+          type="image/svg+xml"
+          data="/section-break-premud.svg"
+        />
+      )}
     </div>
   );
 }
