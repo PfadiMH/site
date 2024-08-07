d83aadaddd9ab5550403ca0f9b74492191dd6f22
Battino <battino46@gmail.com>
2024-08-07 12:43:02 +0000

    Updated Desktop dropdown and added Mobile support

--- components/Navbar/NavbarDropdownComponent.tsx
+++ components/Navbar/NavbarDropdownComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useMemo, useState } from "react";
-import prisma, { Prisma } from "@/lib/prisma";
+import React, { useState } from "react";
+import { Prisma } from "@/lib/prisma";
 
 export type NavbarDropdownProps = Omit<
   Prisma.NavbarDropdownsGetPayload<{}>,
@@ -20,9 +20,9 @@ type Item = {
 };
 
 export function NavbarDropdown({ title, items }: NavbarDropdownProps) {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const changeState = () => {
-    setIsOpen(!isOpen);
+    setOpen(!open);
   };
 
   const groupedItems: Item[][] = items.reduce((groups, item) => {
@@ -39,46 +39,87 @@ export function NavbarDropdown({ title, items }: NavbarDropdownProps) {
 
   return (
     <>
-      <button
-        onClick={changeState}
-        className="text-black text-2xl font-rockingsoda"
-      >
-        {title}
-      </button>
-      {isOpen && (
-        <div className="z-30 -translate-x-10 translate-y-4 drop-shadow-[0px_0px_8px_#000000] absolute items-center bg-white flex flex-col text-black font-rockingsoda text-2xl rounded-xl p-4 border-black b-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 52.36 24.43"
-            className=" absolute w-10 h-10 -translate-x-8 -translate-y-[43.4px] z-20 pointer-events-none"
-          >
-            <path
-              className="fill-white stroke-none"
-              d="M0,22c.18,0,15.99.01,17.77-21.53-.01-.5.67-.65.87-.2,1.72,4.02,5.6,11.55,11.83,15.62,8.27,5.38,16.59,6.13,21.89,6.13,0,.02-50.12-.02-52.36-.02Z"
-            />
-          </svg>
-          {groupedItems.map((items) => (
-            <div className="flex flex-col">
-              {items[0].groups_with ? (
-                <div className="flex space-x-2 items-center">
-                  <div className="w-10 h-1 bg-brand-yellow"></div>
-                  <span className="text-[20px] font-rockingsoda">
-                    {items[0].groups_with}
-                  </span>
-                  <div className="w-10 h-1 bg-brand-yellow"></div>
+      {/* MOBILE */}
+      <div className="md:hidden flex flex-col gap-1">
+        <button
+          onClick={changeState}
+          className="text-brand-yellow text-2xl font-rockingsoda bg-primary  w-full p-3 flex justify-center"
+        >
+          {title}
+        </button>
+        {open && (
+          <div className="pl-2 flex gap-1 flex-col">
+            {groupedItems.map((items) => (
+              <div className="bg-primary w-full p-3 flex flex-col justify-center">
+                <div className="flex w-full gap-2 items-center">
+                  {items[0].groups_with && (
+                    <>
+                      <span className="text-[20px] font-rockingsoda">
+                        {items[0].groups_with}
+                      </span>
+                    </>
+                  )}
                 </div>
-              ) : (
-                <div className="flex space-x-2 items-center">
-                  <div className="w-36 h-1 bg-brand-yellow"></div>
+                <div className="w-full h-[1px] bg-brand-yellow"></div>
+                <div className="flex-col flex">
+                  {items.map((item) => (
+                    <a
+                      className="text-center py-2"
+                      href={item.url || undefined}
+                    >
+                      {item.title}
+                    </a>
+                  ))}
                 </div>
-              )}
-              {items.map((item) => (
-                <a className="text-center">{item.title}</a>
-              ))}
-            </div>
-          ))}
-        </div>
-      )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      {/* DESKTOP */}
+      <div className="hidden md:block">
+        <button
+          onClick={changeState}
+          className="text-black text-2xl font-rockingsoda"
+        >
+          {title}
+        </button>
+        {open && (
+          <div className="z-30 min-w-36 translate-y-4 drop-shadow-[0px_0px_8px_#000000] absolute items-center bg-white flex flex-col text-black font-rockingsoda text-2xl rounded-xl p-4 border-black b-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 52.36 24.43"
+              className=" absolute w-10 h-10 -translate-x-8 -translate-y-[43.4px] z-20 pointer-events-none"
+            >
+              <path
+                className="fill-white stroke-none"
+                d="M0,22c.18,0,15.99.01,17.77-21.53-.01-.5.67-.65.87-.2,1.72,4.02,5.6,11.55,11.83,15.62,8.27,5.38,16.59,6.13,21.89,6.13,0,.02-50.12-.02-52.36-.02Z"
+              />
+            </svg>
+            {groupedItems.map((items) => (
+              <>
+                <div className="flex w-full gap-2 items-center">
+                  <div className="grow h-[3px] bg-brand-yellow"></div>
+                  {items[0].groups_with && (
+                    <>
+                      <span className="text-[20px] font-rockingsoda">
+                        {items[0].groups_with}
+                      </span>
+                      <div className="grow h-[3px] bg-brand-yellow"></div>
+                    </>
+                  )}
+                </div>
+
+                {items.map((item) => (
+                  <a className="text-center" href={item.url}>
+                    {item.title}
+                  </a>
+                ))}
+              </>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 }
