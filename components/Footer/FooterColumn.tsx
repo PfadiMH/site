ed1b768065e71dbaa9042913093aa260dd4273e7
Cuerillius <cyrill_weber@hotmail.com>
2024-08-07 10:02:15 +0000

    style footer

--- components/Footer/FooterColumn.tsx
+++ components/Footer/FooterColumn.tsx
@@ -6,8 +6,8 @@ type FooterColumnProps = Prisma.FooterColumnsGetPayload<{}>;
 
 export async function FooterColumn({ title, content }: FooterColumnProps) {
   return (
-    <div className="">
-      <h2>{title}</h2>
+    <div>
+      <h2 className="pb-2">{title}</h2>
       {content && <WYSIWYG content={content} style={style} />}
     </div>
   );
