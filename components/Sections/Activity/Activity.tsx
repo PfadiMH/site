291c177bb930f7bf70fd40b1a785c9e9b475ef6a
cediackermann <ackermanncedi@gmail.com>
2024-08-10 09:32:49 +0200

    Update Activity component structure

--- components/Sections/Activity/Activity.tsx
+++ components/Sections/Activity/Activity.tsx
@@ -15,12 +15,38 @@ export async function Activity({
 }: ActivityProps) {
   return (
     <section className='bg-background'>
-      {title && <h1>{title}</h1>}
-      {startTime && <p>{startTime.toDateString()}</p>}
-      {startLocation && <p>{startLocation}</p>}
-      {endLocation && <p>{endLocation}</p>}
-      {endTime && <p>{endTime.toDateString()}</p>}
-      {description && <WYSIWYG style={style} content={description} />}
+      <div className='grid grid-rows-1 md:grid-cols-2 gap-4 p-4'>
+        {title && (
+          <h1 className='md:col-span-2 p-4 bg-accent rounded-lg'>{title}</h1>
+        )}
+        {description && (
+          <div
+            className={`md:row-span-${
+              endLocation ? 4 : 3
+            } p-4 bg-accent rounded-lg`}
+          >
+            <WYSIWYG style={style} content={description} />
+          </div>
+        )}
+        {startTime && (
+          <div className='p-4 bg-accent rounded-lg'>
+            <h3>Start Time</h3>
+            <p>{startTime.toDateString()}</p>
+          </div>
+        )}
+        {startLocation && (
+          <p className='p-4 h-[200px] bg-accent rounded-lg'>{startLocation}</p>
+        )}
+        {endTime && (
+          <div className='p-4 bg-accent rounded-lg'>
+            <h3>End Time</h3>
+            <p>{endTime.toDateString()}</p>
+          </div>
+        )}
+        {endLocation && (
+          <p className='p-4 h-[200px] bg-accent rounded-lg'>{endLocation}</p>
+        )}
+      </div>
     </section>
   );
 }
